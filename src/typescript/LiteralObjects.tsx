350f0a12fbb31416fc6d12633074a5e15b7315be
Gerardo Razo Jabana <x30705@gmail.com>
2023-03-05 10:02:52 -0600

    Interfaces.

--- src/typescript/LiteralObjects.tsx
+++ src/typescript/LiteralObjects.tsx
@@ -1,15 +1,44 @@
-import { json } from "stream/consumers"
+interface Person{
+
+    // Use an interface to tell TypeScript how an object looks like.
+
+    name: string;
+    age: number;
+
+    // Prefer tu create another interface for nested objects.
+
+    address: {
+        country: 'Canada',
+        house: number
+    };
+
+    spaceship: Spaceship;
+
+    // Interfaces cannot be used to create instances like:
+    // const person = new Person();
+    // Only for stablishing validation roules to objects. (You should use a class instead).
+}
+
+interface Spaceship{
+    model: String;
+    serial: number;
+}
 
 export const LiteralObjects = () => {
-    
+
     // Literal Objects does not have a literal representation in HTML.
     // With TypeScript, this person object cannot change (add, remove) its attributes.
-    const person = {
+    
+    const person: Person = {
         name: 'Xavier',
         age: 35,
         address: {
             country: 'Canada',
-            number: 615
+            house: 615
+        },
+        spaceship: {
+            model: 'A897',
+            serial: 934802340
         }
     }
 
