99ea70efc2887c25b12b86f3f1a1ac0fdf3e6549
Gerardo Razo Jabana <x30705@gmail.com>
2023-03-05 09:30:58 -0600

    Basic types.

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,7 +1,24 @@
 export const BasicTypes = () => {
+
+    const name: string | number = 'Xavier';
+    const age: number = 35;
+    const active: boolean = false;
+
+    // Array of elemenents of any (mixed) type.
+    const anyPowers = [];
+
+    // Array of strings.
+    const superPowers: string[] = ['flight', 'immortal', 'invisible', 'magic', 'psychic', 'psychokinesis', 'telepathy', 'teleport'];
+    
+    // Array of strings and numbers.
+    const randomPowers: (string | number)[] = [];
+
   return (
     <div className="mt-2">
         <h3>Basic Types</h3>
+        {name}, {age}, {(active) ? 'IS active' : 'IS NOT active'}
+      <br/>
+      {superPowers.join(', ')}
     </div>
   )
 }
