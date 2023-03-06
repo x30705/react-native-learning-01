4cd074c5b5994d5d6aa2c1506dfac8e72e6b2a47
Gerardo Razo Jabana <x30705@gmail.com>
2023-03-06 10:51:39 -0600

    useEffect

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 import { reqResApi } from "../api/reqRes";
+import { ReqResList, User } from '../interfaces/reqResp';
 
 export const Users = () => {
 
     // REQ|RES API: https://reqres.in/
     // AXIOS package for making HTTP requests: https://www.npmjs.com/package/axios
 
+    const [users, setUsers] = useState<User[]>([]);
+
     useEffect(() => {
-        reqResApi.get('/users')
+        reqResApi.get<ReqResList>('/users')
             .then( response => {
                 console.log(response.data.data);
             })
