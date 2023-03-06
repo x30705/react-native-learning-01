6f75a1ed3ede0ef15ea16bb8ddd4af217a9989e7
Gerardo Razo Jabana <x30705@gmail.com>
2023-03-06 10:25:57 -0600

    Axios Call.

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
+import { reqResApi } from "../api/reqRes";
+
 export const Users = () => {
+
+    // REQ|RES API: https://reqres.in/
+    // AXIOS package for making HTTP requests: https://www.npmjs.com/package/axios
+
+    useEffect(() => {
+        reqResApi.get('/users')
+            .then( response => {
+                console.log(response.data.data);
+            })
+            // Same as: .catch(error => console.log(error))
+            .catch(console.log);
+    }, [])
     
   return (
     <>
+        <h3>Users</h3>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Avatar</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     </>
   )
 }
