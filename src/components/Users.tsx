2187388c615990bf8edc362d02c9f19fe6872fc0
Gerardo Razo Jabana <x30705@gmail.com>
2023-03-06 11:14:17 -0600

    Render Users

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -12,12 +12,34 @@ export const Users = () => {
     useEffect(() => {
         reqResApi.get<ReqResList>('/users')
             .then( response => {
-                console.log(response.data.data);
+                setUsers(response.data.data);
             })
             // Same as: .catch(error => console.log(error))
             .catch(console.log);
     }, [])
-    
+
+    // With {field} you can take only the filds you need.
+    const renderItem = ({id, avatar, first_name, email}: User) => {
+        return(
+            // Add key to let React know when an element is removed. ReactNative will need it to be a string.
+            // In img: first { means "Java Scritp Expresssion", second { means "Expression Itself" (style object).
+        <tr key={id.toString()}>
+            <td>
+                <img
+                    src={avatar}
+                    alt={first_name}
+                    style={{
+                        width: 50,
+                        borderRadius: 100
+                    }}
+                />
+            </td>
+            <td>{first_name}</td>
+            <td>{email}</td>
+        </tr>
+        )
+    }
+
   return (
     <>
         <h3>Users</h3>
@@ -30,7 +52,10 @@ export const Users = () => {
                 </tr>
             </thead>
             <tbody>
-
+                {
+                    // Same as: users.map((user: User) => renderItem(user))
+                    users.map(renderItem)
+                }
             </tbody>
         </table>
     </>
